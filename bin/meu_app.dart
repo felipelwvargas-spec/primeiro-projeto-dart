e28123d600fc556615abadad19f19de3e50c5407
Felipe Vargas <felipelwvargas@gmail.com>
2025-09-15 14:12:19 -0300

    Criando funções para operadores matemáticos

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -1,3 +1,24 @@
+import 'dart:io';
+
 void main() {
-  print("Hello World");
+  print("Digite o primeiro numero:");  
+  double numeroUm = double.parse(stdin.readLineSync()!);
+  print("Digite o operador:(Soma + , Subtração - , Divisão / , Multiplicação *)");  
+  var operador = stdin.readLineSync()!;
+  print("Digite o segundo numero:");  
+  double numeroDois = double.parse(stdin.readLineSync()!);
+  // esclamação diz que aquela informação não vai ser null  
+  void soma(){    
+    print(numeroUm + numeroDois);
+  }
+  void subtracao(){
+    print(numeroUm - numeroDois);
+  }
+  void divisao(){
+    print(numeroUm / numeroDois);
+  }
+  void multiplicacao(){
+    print(numeroUm * numeroDois);
+  }
+
 }
