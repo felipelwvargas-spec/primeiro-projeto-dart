4b79d3b9fe9f0ac0e17d621688ff88c7a3ae723a
Felipe Vargas <felipelwvargas@gmail.com>
2025-09-15 17:43:18 -0300

    Criando funções para operadores matemáticos 0.1

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -1,24 +1,74 @@
 import 'dart:io';
 
 void main() {
-  print("Digite o primeiro numero:");  
-  double numeroUm = double.parse(stdin.readLineSync()!);
-  print("Digite o operador:(Soma + , Subtração - , Divisão / , Multiplicação *)");  
-  var operador = stdin.readLineSync()!;
-  print("Digite o segundo numero:");  
-  double numeroDois = double.parse(stdin.readLineSync()!);
-  // esclamação diz que aquela informação não vai ser null  
-  void soma(){    
-    print(numeroUm + numeroDois);
-  }
-  void subtracao(){
-    print(numeroUm - numeroDois);
-  }
-  void divisao(){
-    print(numeroUm / numeroDois);
-  }
-  void multiplicacao(){
+  double numeroUm = 0;
+
+  String operacao = "";
+
+  double numeroDois = 0;
+
+  void multiplicacao() {
     print(numeroUm * numeroDois);
   }
 
+  void soma() {
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao() {
+    print(numeroUm - numeroDois);
+  }
+
+  void divisao() {
+    print(numeroUm / numeroDois);
+  }
+
+  print("Digite o primeiro numero:");
+  String? entrada = stdin.readLineSync();
+
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroUm = double.parse(entrada);
+    }
+  }
+  print("Digite o operador:");
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    operacao = entrada;
+  }
+  print("Digite o segundo numero:");
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroDois = double.parse(entrada);
+    }
+  }
+
+  switch (operacao) {
+    case "+":
+      soma();
+    case "-":
+      subtracao();
+    case "*":
+      multiplicacao();
+    case "/":
+      divisao();
+      break;
+  }
 }
+
+  
+/*
+  
+}
+import 'dart:io';
+
+void main() {
+  print("Digite um numero:");  
+  double numeroUm = double.parse(stdin.readLineSync()!);
+   void multiplicacao(){
+    print("O dobro do numero ${numeroUm} é ${numeroUm * 2}");
+  }
+  multiplicacao();
+} 
+*/
